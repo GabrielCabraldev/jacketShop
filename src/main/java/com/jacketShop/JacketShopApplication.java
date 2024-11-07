3c557277f5b4fc1c371bfd0868e901ee1275ea6d
Gabriel Cabral <gabriel1999sp@gmail.com>
2024-11-07 18:57:39 -0300

    Initial commit

---
+++ src/main/java/com/jacketShop/JacketShopApplication.java
@@ -0,0 +1,13 @@
+package com.jacketShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacketShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacketShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacketShop/JacketShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacketShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacketShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
